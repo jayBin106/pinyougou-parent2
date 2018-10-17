73c1ba538020850dc3ced75556691c0448fbae89
Felix <fly.mrfeng@foxmail.com>
2018-10-17 16:47:55 +0800

    第四天完成，shopweb的权限控制完成

--- pinyougou-manager-web/src/main/java/com/pinyougou/controller/LoginController.java
+++ pinyougou-manager-web/src/main/java/com/pinyougou/controller/LoginController.java
@@ -1,13 +1,27 @@
 package com.pinyougou.controller;
 
+import com.pinyougou.until.DateUtil;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by lenovo on 2018/10/16.
  */
-//@Controller
-//@RequestMapping("/login")
-//public class LoginController {
-//    @RequestMapping("/")
-//    public String security() {
-//        return "/admin/index";
-//    }
-//}
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+    @RequestMapping("/name")
+    public Map name() {
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        HashMap<String, String> map = new HashMap<>();
+        map.put("loginName", name);
+        map.put("lastTime", DateUtil.dateFormat(new Date(), DateUtil.sdf3));
+        return map;
+    }
+}
