2597870d6af29a3557cb22ad9a6b287a3a887e73
Felix <fly.mrfeng@foxmail.com>
2018-10-26 18:54:35 +0800

    第8天，广告

---
+++ pinyougou-portal-web/src/main/java/com/pinyougou/PinyougouPortalWebApplication.java
@@ -0,0 +1,12 @@
+package com.pinyougou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinyougouPortalWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinyougouPortalWebApplication.class, args);
+	}
+}
