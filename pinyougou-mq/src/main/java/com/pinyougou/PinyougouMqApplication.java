0ae92eab5dc40bff0a05ed1b4ccecd0634fe780b
Felix <fly.mrfeng@foxmail.com>
2018-11-04 20:19:02 +0800

    商品详情页问题解决，打包问题解决

---
+++ pinyougou-mq/src/main/java/com/pinyougou/PinyougouMqApplication.java
@@ -0,0 +1,11 @@
+package com.pinyougou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinyougouMqApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PinyougouMqApplication.class, args);
+	}
+}
